6c6638f567ef5c03e9be2e256069f9717ea3b7bc
omer <omersyed861@gmail.com>
2023-01-18 19:27:43 +0000

    getting started with git

---
+++ src/main/java/com/dlf/www/App.java
@@ -0,0 +1,13 @@
+package com.dlf.www;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "firstly i am going to compile,test,package using jenkin" );
+    }
+}
